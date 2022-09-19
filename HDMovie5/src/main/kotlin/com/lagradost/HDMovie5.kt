f3e15903a08dc6404fb8fab8cb9c4d2f9da66483
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-09-19 13:56:09 +0530

    Update HDMovie5.kt (#22)

--- HDMovie5/src/main/kotlin/com/lagradost/HDMovie5.kt
+++ HDMovie5/src/main/kotlin/com/lagradost/HDMovie5.kt
@@ -9,7 +9,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class HDMovie5 : MainAPI() {
-    override var mainUrl = "https://hdmovie2.run"
+    override var mainUrl = "https://hdmovie2.bond"
     override var name = "HDMovie"
     override var lang = "hi"
 
