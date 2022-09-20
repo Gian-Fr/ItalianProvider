73d3cfc8b858dd60ffd6cf9a893a347980b0ef23
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-09-21 04:04:03 +0530

    Update domain  (#24)
    * Update HDMovie5.kt
    
    * Update YomoviesProvider.kt

--- HDMovie5/src/main/kotlin/com/lagradost/HDMovie5.kt
+++ HDMovie5/src/main/kotlin/com/lagradost/HDMovie5.kt
@@ -9,7 +9,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class HDMovie5 : MainAPI() {
-    override var mainUrl = "https://hdmovie2.bond"
+    override var mainUrl = "https://hdmovie2.rip"
     override var name = "HDMovie"
     override var lang = "hi"
 
