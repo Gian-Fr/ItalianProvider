94462ea208ccf6b4f5f2b9e31d307c9732ef9c0f
contusionglory <102427829+contusionglory@users.noreply.github.com>
2022-12-08 12:10:06 +0000

    Add CasaCinemaProvider (#64)

---
+++ CasaCinemaProvider/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 2
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+    language= "it"
+    authors = listOf("Forthe")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+}
