dc4ca1991f5101bd7ffce277fcf525f9b715e994
Diego Sánchez <zenit4d@gmail.com>
2022-12-12 05:21:35 -0300

    Repair last chapters bug in MundoDonghua Provider (#67)

--- MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
@@ -36,9 +36,10 @@ class MundoDonghuaProvider : MainAPI() {
                     val title = it.selectFirst("h5")?.text() ?: ""
                     val poster = it.selectFirst(".fit-1 img")?.attr("src")
                     val epRegex = Regex("(\\/(\\d+)\$)")
-                    val url = it.selectFirst("a")?.attr("href")?.replace(epRegex,"")?.replace("/ver/","/donghua/")
                     val epnumRegex = Regex("((\\d+)$)")
                     val epNum = epnumRegex.find(title)?.value?.toIntOrNull()
+                    val epNumRemoveRegex = Regex("/" + epNum.toString() + "/.*")
+                    val url = it.selectFirst("a")?.attr("href")?.replace(epRegex,"")?.replace("/ver/","/donghua/")?.replace(epNumRemoveRegex,"")
                     val dubstat = if (title.contains("Latino") || title.contains("Castellano")) DubStatus.Dubbed else DubStatus.Subbed
                     newAnimeSearchResponse(title.replace(Regex("Episodio|(\\d+)"),"").trim(), fixUrl(url ?: "")) {
                         this.posterUrl = fixUrl(poster ?: "")
