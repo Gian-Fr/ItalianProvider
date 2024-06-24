06591b6e3791b8c3f354a1739c947a6d5f862e7d
gianfr <63294205+Gian-Fr@users.noreply.github.com>
2024-06-24 15:50:52 +0200

    Updated SoraItalianStream urls

--- SoraItalianStream/src/main/kotlin/SoraItalianStream.kt
+++ SoraItalianStream/src/main/kotlin/SoraItalianStream.kt
@@ -35,12 +35,12 @@ open class SoraItalianStream : TmdbProvider() {
     companion object {
         private const val tmdbAPI = "https://api.themoviedb.org/3"
         private const val apiKey = "71f37e6dff3b879fa4656f19547c418c" // PLEASE DON'T STEAL
-        const val guardaserieUrl = "https://guardaserie.black"
-        const val filmpertuttiUrl = "https://www.filmpertutti.wine/"
-        const val cb01Url = "https://cb01.mobi/"
+        const val guardaserieUrl = "https://guardaserie.ceo"
+        const val filmpertuttiUrl = "https://www.filmpertutti.casino/"
+        const val cb01Url = "https://cb01.in/"
         const val animeworldUrl = "https://www.animeworld.so"
-        const val aniplayUrl = "https://aniplay.co"
-        const val animesaturnUrl = "https://www.animesaturn.tv/"
+        const val aniplayUrl = "https://aniplaynow.live/"
+        const val animesaturnUrl = "https://www.animesaturn.nl/"
         const val tmdb2mal = "https://tmdb2mal.slidemovies.org"
         fun getType(t: String?): TvType {
             return when (t) {
