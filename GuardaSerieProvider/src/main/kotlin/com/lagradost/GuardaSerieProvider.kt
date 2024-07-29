05a18a2155389926d066f38814857417f70d2708
gianfr <63294205+Gian-Fr@users.noreply.github.com>
2024-07-29 20:10:12 +0200

    Try to fix GuardaSerieProvider cover in search

--- GuardaSerieProvider/src/main/kotlin/com/lagradost/GuardaSerieProvider.kt
+++ GuardaSerieProvider/src/main/kotlin/com/lagradost/GuardaSerieProvider.kt
@@ -39,7 +39,7 @@ class GuardaSerieProvider : MainAPI() {
                 TvType.Movie
             ) {
                 this.posterUrl = posterUrl
-                this.posterHeaders = mapOf("user-agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+                this.posterHeaders = interceptor.getCookieHeaders(mainUrl).toMap()
             }
 
         }
